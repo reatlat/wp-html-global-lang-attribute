d7e96ed66c212e058cf1440c66b52ef0ecc35884
Alex Zappa <reatlat@gmail.com>
2018-06-09 23:10:25 +0200

    WIP

--- html-global-lang-attribute/html-global-lang-attribute.php
+++ html-global-lang-attribute/html-global-lang-attribute.php
@@ -4,14 +4,14 @@
  * Plugin URI:        https://github.com/reatlat/wp-html-global-lang-attribute
  * Description:       description
  * Tags:              tags
- * Version:           0.0.1
+ * Version:           0.0.2
  * Author:            Alex Zappa a.k.a. re[at]lat
  * Author URI:        https://reatlat.net
  * Donate link:       https://www.paypal.me/reatlat/5usd
  * Requires at least: 2.1.0
  * Tested up to:      4.9.6
  * Requires PHP:      5.6 or later
- * Stable tag:        0.0.1
+ * Stable tag:        0.0.2
  * License:           GPL-3.0+
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
  * Text Domain:       html-global-lang-attribute
@@ -27,7 +27,22 @@ if ( ! defined( 'WPINC' ) )
 
 function activate_htmlGlobalLangAttribute()
 {
+    // TODO: activate hooks
+}
 
+
+function deactivate_htmlGlobalLangAttribute()
+{
+    // TODO: deactivate hooks
+}
+
+
+register_activation_hook( __FILE__, 'activate_htmlGlobalLangAttribute' );
+register_deactivation_hook( __FILE__, 'deactivate_htmlGlobalLangAttribute' );
+
+
+if ( ! function_exists( 'set_HtmlGlobalLangAttribute' ) )
+{
     function set_HtmlGlobalLangAttribute( $output )
     {
         $lang = substr( get_locale(), 0, 2);
@@ -40,25 +55,7 @@ function activate_htmlGlobalLangAttribute()
 
         return $output;
     }
-
     add_filter( 'language_attributes', 'set_HtmlGlobalLangAttribute');
-
 }
-
-
-function deactivate_htmlGlobalLangAttribute()
-{
-
-    // TODO: remove some settings and options
-
-}
-
-
-register_activation_hook( __FILE__, 'activate_htmlGlobalLangAttribute' );
-register_deactivation_hook( __FILE__, 'deactivate_htmlGlobalLangAttribute' );
-
-
-
-
 
 
